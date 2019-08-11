598dd500b43d0341d9f94387e2ff7bf103548b09
ThomasPTurner <ThomasPTurner@googlemail.com>
2019-08-11 17:43:10 +0100

    fix database seeding

--- backend/db/migrations/20190811164602_bugs.js
+++ backend/db/migrations/20190811164602_bugs.js
@@ -2,10 +2,10 @@
 exports.up = function(knex) {
   return knex.schema.createTable('bugs', bugsTable => {
       bugsTable.increments('id').primary().unique();
-      bugsTable.string('title').primart().notNullable();
+      bugsTable.string('title').notNullable();
       bugsTable.string('body', 5000).notNullable();
       bugsTable.boolean('open').defaultsTo(true)
-      bugsTable.integer('asssigned_to').defaultsTo(null).references(users.id)
+      bugsTable.integer('assigned_to').defaultsTo(null).references('users.id')
       bugsTable.timestamp('created_at').defaultsTo(knex.fn.now())
   })
 };
---
+++ backend/db/setup.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS bug_tracker_test;
+DROP DATABASE IF EXISTS bug_tracker
+
+CREATE DATABASE bug_tracker_test;
+CREATE DATABASE bug_tracker;
