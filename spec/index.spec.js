e96b80ee9df1cb146910fa5910961ccfc29fd755
thomaspturner <thomaspturner@googlemail.com>
2019-08-11 15:51:20 +0100

    write first server test

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,2 +1,18 @@
 const { expect } = require('chai')
-const connection = require('supertest')() 
+chai.use('chai-sorted')
+const app = require('../backend/app')
+const request = require('supertest')(app) 
+
+describe('Bug Tracker App', ()=> {
+    describe('/api', () => {
+        describe('/bugs', ()=> {
+            describe('GET', ()=> {
+                it('200 on request to endpoint', ()=> {
+                    return request
+                        .get('/api/bugs')
+                        .expect(200)
+                })
+            })
+        })
+    })
+})
