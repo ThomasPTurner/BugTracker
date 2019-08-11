4439bd6203c2f5d6fdb811ca205756dca6ef99d1
ThomasPTurner <ThomasPTurner@googlemail.com>
2019-08-11 19:39:55 +0100

    front end displaying bug cards

--- frontend/bug-tracker/src/App.js
+++ frontend/bug-tracker/src/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import './App.css'
+import React, { Component } from 'react';
+import axios from 'axios'
+import moment from 'moment';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+const BASE_URL = 'http://localhost:9090/api'
+
+class App extends Component {
+  state = {
+    bugs : []
+  }
+  render() {
+    const {bugs} = this.state
+    return (
+      <div className="App">
+        <h3 className="header">Bug Tracker</h3>
+        <body className="body">
+          {
+            bugs.map(({id, title, body, created_at, assigned_to, open}) => {
+              return (
+                <div className='bugCard' key={`bug:${id}`}>
+                  <p>Title: {title}</p>
+                  <p>Description: {body}</p>
+                  <p>{assigned_to ? `Assigned to ${assigned_to}` : 'Unassigned' }</p>
+                  <p>{created_at}</p>
+                  <p>Status: {open ? 'open' : 'closed' }</p>
+                </div>
+              )
+            })
+          }
+        </body>
+        <p className="footer">Created By Thomas Turner</p> 
+      </div>
+    );
+  }
+  componentDidMount() {
+    this.getBugs()
+  }
+
+  getBugs = async () => {
+    const {data: {bugs}} = await axios.get(`${BASE_URL}/bugs`)
+    this.setState({ bugs })
+  }
 }
 
 export default App;
